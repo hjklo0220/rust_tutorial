c759076e885600167be413b967405fa63e53f2b1
jun <hjklo0220@gmail.com>
2025-03-27 00:32:44 +0900

    3-4: control flow

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -91,7 +91,7 @@ fn main() {
     another_function(5);
     print_labeled_measurement(10, 'h');
 
-    let statement = 6;
+    // let statement = 6;
     // let statement1 = (let statement2 = 6);
     let expression = {
         let statement = 3;
@@ -104,6 +104,97 @@ fn main() {
 
     let plus_number = plus_one(five);
     println!("{plus_number}");
+
+
+    let number = 6;
+    
+    if number < 5 {
+        println!("true");
+    } else {
+        println!("false");
+    }
+
+    if number != 0 {
+        println!("0 -> false? no!");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 1 } else { 0 };
+    println!("{number}");
+
+    // if condition {
+    //     1
+    // } else {
+    //     0
+    // }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("loop result: {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        
+        count += 1;
+
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+
+    println!("LIFEOFF!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut i = 0;
+
+    while i < 5 {
+        println!("value = {}", a[i]);
+        i += 1;
+    }
+
+    for element in a {
+        println!("in for loop value = {element}");
+    }
+
+    for n in (1..4).rev() {
+        println!("n = {n}")
+    }
 
 }
 
