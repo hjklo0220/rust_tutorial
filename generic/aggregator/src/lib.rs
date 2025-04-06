f896c5437fb38cec7ba2d33abcaf7be337d34734
jun <hjklo0220@gmail.com>
2025-04-07 00:33:55 +0900

    10-2: trait

---
+++ generic/aggregator/src/lib.rs
@@ -0,0 +1,101 @@
+use std::fmt::Display;
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+}
+
+pub fn notify<T: Summary + Display>(item1: &T, item2: &T) {
+    println!("Breaking news! {} | {}", item1.summarize(), item2.summarize_author());
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    }
+    // if switch {
+    //     NewsArticle {
+    //         headline: String::from(
+    //             "Penguins win the Stanley Cup Championship!",
+    //         ),
+    //         location: String::from("Pittsburgh, PA, USA"),
+    //         author: String::from("Iceburgh"),
+    //         content: String::from(
+    //             "The Pittsburgh Penguins once again are the best \
+    //              hockey team in the NHL.",
+    //         ),
+    //     }
+    // } else {
+    //     Tweet {
+    //         username: String::from("horse_ebooks"),
+    //         content: String::from(
+    //             "of course, as you probably already know, people",
+    //         ),
+    //         reply: false,
+    //         retweet: false,
+    //     }
+    // }
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
--- generic/src/main.rs
+++ generic/src/main.rs
@@ -3,6 +3,8 @@ mod largest;
 use crate::largest::func;
 use crate::largest::point;
 
+use aggregator::{Summary, Tweet, NewsArticle};
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = func::largest(&number_list);
@@ -34,5 +36,39 @@ fn main() {
 
     let integer = Some(5);
     let float = Some(5.0);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
 }
+
 
