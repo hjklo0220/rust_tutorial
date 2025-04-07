b8f45541138c3cf672fd6e1577f2a91d2fdf893a
jun <hjklo0220@gmail.com>
2025-04-08 00:25:51 +0900

    10-3-2: struct, method, static lifetime

--- generic/lifetime/src/lib.rs
+++ generic/lifetime/src/lib.rs
@@ -18,16 +18,72 @@ pub fn lifetime_main() {
         let result2 = longest(string3.as_str(), string4.as_str());
         println!("The longest string is {result2}");
     }
-    let string5 = String::from("long string is long long long");
-    let result3;
-    {
-        let string6 = String::from("abc");
-        result3 = longest(string5.as_str(), string6.as_str());
-    }
-    println!("{result3}");
+
+    // let string5 = String::from("long string is long long long");
+    // let result3;
+    // {
+    //     let string6 = String::from("abc");
+    //     result3 = longest(string5.as_str(), string6.as_str()); // no compile
+    // }
+    // println!("{result3}");
+
+    let novel = String::from("call me Ishmael. some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    let s = "abc def";
+    println!("s: {}", first_word(s));
+
+    let static_lifetime: &'static str = "I have a static lifetime.";
 }
 
-fn longest<'lifetime>(x: &'lifetime str, y: &'lifetime str) -> &'lifetime str {
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn first_word<'a>(s: &'a str) -> &'a str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
