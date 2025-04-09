addcc4d68760a7c6dae241671f3900dc33b4f9b5
jun <hjklo0220@gmail.com>
2025-04-10 00:57:34 +0900

    12-5: use env

--- minigrep/src/tests/search_tests.rs
+++ minigrep/src/tests/search_tests.rs
@@ -1,12 +1,25 @@
 use crate::*;
 
 #[test]
-fn test_search_function() {
+fn case_sensitive() {
     let query = "duct";
     let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
     assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
 }
