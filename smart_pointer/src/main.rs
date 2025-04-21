3215375c08c4526844401532910d5f9d20d04655
jun <hjklo0220@gmail.com>
2025-04-22 00:24:07 +0900

    15-1: Box<T>

---
+++ smart_pointer/src/main.rs
@@ -0,0 +1,20 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
